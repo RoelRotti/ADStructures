1b03342fb016a8c0df5fec38ca16d9be582c4b04
Mathijs <mathew9753@gmail.com>
2017-02-14 12:13:33 +0100

    first commit.0

---
+++ anagram.c
@@ -0,0 +1,40 @@
+/* file: anagram.c                                      */
+/* author: A.M van Maurik (email: mathew9753@gmail.com) */
+/* date: 14-02-2017                                     */
+/* version: 1.00                                        */
+/* Description:
+*/
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+int main(int argc, char *argv[]) {
+	// Variables
+	int n;
+	int m;
+
+	// The first integer input is the number of sentences in the table.
+	scanf("%d",&n);
+
+	//Looping through n sentences.!  Sentence ends with "." Full stop. 
+
+	// The second integer input is the number of test sentences.
+	scanf("%d",&m);
+
+	//Looping through m test sentences.! Sentence ends with "." Full stop. 
+
+
+
+	// PROGRAM! FIRST DETERMINE WICH SENTENCES HAVE THE SAME LENGTH (WITHOUT SPACING)
+	
+
+	// Testing everything.
+	printf("Hello World!\n");
+	printf("table sentences: %d and testing sentences: %d\n", n, m);
+
+	return 0;
+}
